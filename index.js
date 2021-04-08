98b86618b5dab0736b2bb58a6978d6fd98743d03
Domninik Schenk <domi.schenk@gmail.com>
2021-04-08 23:09:29 +0200

    init

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-function init() {
+function init(value) {
     const elem = document.createElement('div');
-    elem.innerText = 'Hallo Welt';
+    elem.innerText = value;
 
     document.body.appendChild(elem);
 }
 
-init();
+
+
+
+async function gitCommand() {
+    const args = [
+        '-C',
+        'D:/_dev/_repotest/branch',
+        'diff',
+        '--no-ext-diff',
+        '--patch-with-raw',
+        '--no-color',
+        '--no-index',
+        '--',
+        '/dev/null',
+        'gugs.md'
+    ]
+    const cmd = new __TAURI__.shell.Command('git', args);
+    // cmd.execute();
+    const result = await cmd.execute();
+    console.log(result);
+    init(result.stdout)
+}
+
+gitCommand();
